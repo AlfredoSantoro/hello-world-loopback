55d111232850601563dec6aa693261a5b59c69ea
Alfredo Santoro <alfredo.santoro-cic-it@ibm.com>
2020-02-04 11:08:17 +0100

    findByname and findBysurname added

--- common/models/people.js
+++ common/models/people.js
@@ -1,5 +1,80 @@
 'use strict';
 
-module.exports = function(People) {
+const Logger = require('winston');
 
+module.exports = function(People) {
+  People.findByName = function(name, cb) {
+    Logger.info('received get request /find-by-name');
+    People.find(
+      {
+        where: {
+          name: name,
+        },
+      }, function(err, result) {
+      if (err) {
+        Logger.info('the following error has occurred: ' + err);
+        cb(null, err);
+        return;
+      }
+      Logger.info('get request /find-by-name Done..');
+      return cb(null, result);
+    }
+    );
+  };
+  People.findBySurname = function(surname, cb) {
+    Logger.info('received get request /find-by-surname');
+    People.find(
+      {
+        where: {
+          surname: surname,
+        },
+      }, function(err, result) {
+      if (err) {
+        Logger.info('the following error has occurred: ' + err);
+        cb(null, err);
+        return;
+      }
+      Logger.info('get request /find-by-surname Done..');
+      return cb(null, result);
+    }
+    );
+  };
+  /* A remote method is a static method of a model, exposed over a custom REST endpoint */
+  People.remoteMethod(
+    'findByName',
+    {
+      description: 'Find a people by name',
+      http: {path: '/find-by-name', verb: 'get'},
+      accepts: [
+        {
+          arg: 'name',
+          type: 'string',
+          http: {
+            source: 'query',
+          },
+          required: true,
+        },
+      ],
+      returns: {arg: 'result', type: 'array'},
+    }
+  );
+
+  People.remoteMethod(
+    'findBySurname',
+    {
+      description: 'Find a people by surname',
+      http: {path: '/find-by-surname', verb: 'get'},
+      accepts: [
+        {
+          arg: 'surname',
+          type: 'string',
+          http: {
+            source: 'query',
+          },
+          required: true,
+        },
+      ],
+      returns: {arg: 'result', type: 'array'},
+    }
+  );
 };
